a8eaf9f036f8fd9d239b5d4dedfcbaa1805e2c90
boss10801 <boss101801@gmail.com>
2023-12-19 23:45:00 +0700

    Enable to check the key file

--- EnDeTypoMk2.py
+++ EnDeTypoMk2.py
@@ -1,8 +1,27 @@
+import os
+
+keyFilePath = 'key.txt'
+
+def isEmpty(keyFile):
+    if (os.path.getsize(keyFile) == 0):
+        print("Error can't open key file.(terminate Program)\n")
+        exit(1)
+
 def Cesar(Input, Type):
-    with open('key.txt') as keyfile:
-        key = keyfile.read()
+    # Check If the file is empty
+    isEmpty(keyFilePath)
+    with open(keyFilePath) as keyFile:
+        key = keyFile.read()
     lenght = len(key)
-    print(key)
-    print(lenght)
+    # For Testing
+    # print(key)
+    # print(lenght)
+    if (Type == 1): #Encrypt
+        i = 0
+        j = 0
+        print("i=",i,"j=",j)
+
+
+    keyFile.close
 
 Cesar(1,1)
