d823d514f6551891835141b1aee26260a7c5202d
boss10801 <boss101801@gmail.com>
2023-12-20 01:07:09 +0700

    create a new function make it able to Encrypt

--- EnDeTypoMk2.py
+++ EnDeTypoMk2.py
@@ -16,11 +16,30 @@ def Cesar(Input, Type):
     # For Testing
     # print(key)
     # print(lenght)
+    InputList = list(Input)
     if (Type == 1): #Encrypt
         i,j = 0,0
-        print("i=",i,"j=",j)
+        for i in range(len(InputList)):
+            if(j == lenght):
+                j = 0
+            # Convert InputList to ASCII
+            ascii_value = ord(InputList[i])
+            #print("ascii of input:",ascii_value)
 
+            # Make it in range of ASCII from 32 to 126 
+            # which will be space to ~
+            encrypted_value = (ascii_value + int(key[j])) % (126 - 32 + 1) + 32
+            #print("Encrypted input:", encrypted_value)
+            
+            # Convert back to char
+            InputList[i] = chr(encrypted_value)
+            j += 1
 
+        # Convert InputList(List) back to Input(string)
+        Input = ''.join(InputList)
+        print("<Encrypt>"+Input+"<Encrypt>")
+        
     keyFile.close
 
-Cesar(1,1)
+
+Cesar("Sussy oil",1)
